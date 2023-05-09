2f2dce55d668151fdf6cc1fa084c0cbd97ecaaf6
Goidan Matei-Constantin <128105007+MateiGoidan@users.noreply.github.com>
2023-05-10 01:15:53 +0300

    Update light_jet.hpp

--- light_jet.hpp
+++ light_jet.hpp
@@ -1,22 +1,28 @@
 #ifndef LIGHT_JET
 #define LIGHT_JET
 
-#include <string>
 #include "plane.hpp"
 
 class Light_Jet : public Plane
 {
 public:
-    Light_Jet(std::string al);
-    virtual int Speed();
-    virtual int Altitude();
+    Light_Jet(std::string al) : airline(al) {}
+
+    virtual int Speed() {
+        return max_speed;
+    }
+
+    virtual int Altitude() 
+    {
+        return max_altitude;
+    }
 
 private:
-    std::string airline_agency;
+    std::string airline;
     int seats = 7;
-    int max_speed = 700;                        // km/h
-    int max_altitude = 13716;                   // meters
-
+    int max_speed = 70;
+    int max_altitude = 1400;
+    
 };
 
 #endif // !LIGHT_JET
