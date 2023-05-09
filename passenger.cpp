c6b69c0bf49933d7ae071e468dbae658c1ed0422
Goidan Matei-Constantin <128105007+MateiGoidan@users.noreply.github.com>
2023-05-10 01:20:20 +0300

    Update passenger.cpp

--- passenger.cpp
+++ passenger.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "document.hpp"
 #include "data.hpp"
 #include "passenger.hpp"
 
@@ -8,3 +9,18 @@ long Passenger::id = 1000;
 //Constructors
 Passenger::Passenger(std::string n, std::string e, std::string p, std::string t, std::string s, long id, int ed) : Data(n, e, p), Document(t,s,id,ed) {}
 
+//Getters and Setters
+long Passenger::Get_ID()
+{
+    return id;
+}
+
+std::string Passenger::Get_Name()
+{
+    return name;
+}
+
+void Passenger::Set_Status(std::string s)
+{
+    status = s;
+}
