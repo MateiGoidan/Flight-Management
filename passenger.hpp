beec4f4b32bd825998b07711c9eb96345909dcca
Goidan Matei-Constantin <128105007+MateiGoidan@users.noreply.github.com>
2023-05-10 01:20:56 +0300

    Update passenger.hpp

--- passenger.hpp
+++ passenger.hpp
@@ -8,9 +8,13 @@ class Passenger : protected Data, protected Document
 {
     public:
         Passenger(std::string n, std::string e, std::string p, std::string t, std::string s, long id, int ed);          //Constructor the passenger
+        void Set_Status(std::string s);
+        static long Get_ID();
+        std::string Get_Name();
                                                                                                                         
     private:
         static long id;                                                                                                  //The passenger ID
+        std::string status;
 };
 
 #endif // !PASSENGER
