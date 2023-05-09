cf0bd7b1a33fd09a961820c60a2b661d25358729
Goidan Matei-Constantin <128105007+MateiGoidan@users.noreply.github.com>
2023-05-10 01:09:16 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -18,15 +18,41 @@ Data::Data(std::string n, std::string e, std::string p) : name(n), email(e), pho
         return 0;
     };
 
+    //Implementing a lambda function to verify if n contains only letters
+    auto verfa = [](const std::string &s) {
+        for(char c : s)
+            if(std::isalpha(c) == 0)
+                return 1;
+        return 0;
+    };
+
     //Veryfing if both the email address and the phone number are corect
+    //Veryfing if the name is corect
+    try {
+        if(verfa(name)) {
+            ex = "Not a real name!\n";
+            throw std::runtime_error(pb);
+        }
+    }
+    catch (const std::runtime_error &e) {
+        std::cout << e.what() << std::endl;
+        try {
+            throw Exception(ex); 
+        } 
+
+        catch(Exception &e) {
+            std::cout << e.what() << std::endl;
+        }
+    }
+
     //Veryfing email address
     try {
-        if(email.find('@') == std::string::npos) {
+        if(email.find("@") == std::string::npos) {
             ex = "Missing '@'.\n";
             throw std::runtime_error(pb);
         }
 
-        if(email.find(".com") == std::string::npos || email.find(".ro") == std::string::npos) {
+        if(email.find("com") == std::string::npos || email.find("ro") == std::string::npos) {
             ex = "Mising top-level domain (ex: .com).\n";
             throw std::runtime_error(pb);
         }
